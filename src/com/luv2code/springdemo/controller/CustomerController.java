4ab3c6c845b669385a957d20ea89cb10fabb9a37
ysz951 <shengyang_zhou@hotmail.com>
2020-12-19 17:51:03 -0600

    Add form input

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -25,6 +25,8 @@ public class CustomerController {
 
     @GetMapping("showFormForAdd")
     public String showFormForAdd(Model theModel) {
+        Customer theCustomer = new Customer();
+        theModel.addAttribute("customer", theCustomer);
         return "customer-form";
     }
 }
